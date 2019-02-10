099bbfbc119250b5e35ac81a9aecbd17775ba7b5
Marc <marc.purgimon@gmail.com>
2019-02-11 00:13:59 +0100

    Crida inicial, pendent

--- servidor.js
+++ servidor.js
@@ -8,71 +8,107 @@ var app = express();
     3- Trèbol  ♣
 */
 
-var jugador = {
-    nom : null,
-    intents : 0,
 
-}
 
 /* cartes -> codi baralla i valor */
-function jugador(nom, cartes) {
+function jugador(nom) {
     this.nom = nom;
     this.intents = 0;
-    this.cartes = cartes;
     this.victories = victories;
   }
 
 var partida = {
-    id= null,
-    baralles = Array(
+    id : 0,
+    baralles : Array(
         0,
         1,
         2,
         3
     ),
 
-    barallaCartes = [
+    barallaCartes : [
         [0 ,  ["A","Q","K","J","10","9","8","7","6","5","4","3","2"]], // Cors ♥
-        1, ["A","Q","K","J","10","9","8","7","6","5","4","3","2"], // Diamants ♦
-        2, ["A","Q","K","J","10","9","8","7","6","5","4","3","2"], // Piques ♠
-        3 = ["A","Q","K","J","10","9","8","7","6","5","4","3","2"], // Trèbol  ♣
+        [1, ["A","Q","K","J","10","9","8","7","6","5","4","3","2"]], // Diamants ♦
+        [2, ["A","Q","K","J","10","9","8","7","6","5","4","3","2"]], // Piques ♠
+        [3 , ["A","Q","K","J","10","9","8","7","6","5","4","3","2"]], // Trèbol  ♣
 
     ],
 
-    jugadors = Array(
+    jugadors : Array(
 
     ),
 
 
-    tauler = Array(),
+    tauler : Array(),
 
        
     
     
 };
 
-app.post('iniciarJoc/:codiPartida/nomJugador/:nom', function (req, res, next){
-        res.send("Rebut: "+req.param.nom);
+
+
+
+    
+function repartirCartes(jugador){
+
+    var baralla = 4; //Num de baralles
+    var cartes = 13;    //Num de cartes
+
+
+    /* Random de la carta */
+   var bEscollida =Math.floor(Math.random()*baralla);
+   var cEscollida =Math.floor(Math.random()*cartes);
+
+   var bcarta = partida.barallaCartes[bEscollida];
+   var carta = partida.barallaCartes[cEscollida];
+
+    jugador.cartes[bcarta][carta];
+
+}
+
+
+
+
+
+app.get('/iniciarjoc/:id/:nom', function (req, res, next){
+
+        partida.id = 1;
+        
+        var jugadorA = {
+            nom: (req.params.nom),
+            cartes: [],
+            intents: 0,
+            victories: 0,
+        };
+
+        console.log(jugadorA.cartes);
+
+        repartirCartes(jugadorA);
+
+
+        res.send("Rebut: "+req.params.nom);
+        res.send("Rebut: "+req.params.id);
 });
 
 
-app.get('obtenirCarta/:codiPartida', function (req, res, next){
+app.get('/obtenirCarta/:codiPartida', function (req, res, next){
 
 });
 
-app.get('mostrarCartes/:codiPartida', function (req, res, next){
+app.get('/mostrarCartes/:codiPartida', function (req, res, next){
 
 });
 
-app.put('tirarCarta/:codiPartida/:carta', function (req, res, next){
+app.put('/tirarCarta/:codiPartida/:carta', function (req, res, next){
 
 });
 
-app.put('moureJugador/:codiPartida/aposta/quantitat', function (req, res, next){
+app.put('/moureJugador/:codiPartida/aposta/quantitat', function (req, res, next){
 
 });
 
-app.put('moureJugador/:codiPartida/passa', function (req, res, next){
+app.put('/moureJugador/:codiPartida/passa', function (req, res, next){
 
 });
 
